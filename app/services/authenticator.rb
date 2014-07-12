4d64015d5102de57ff436eff346b176bdf402759
jizoo <jizoo2200@gmail.com>
2014-07-13 08:54:05 +0900

    Add suspended checkbox

--- app/services/authenticator.rb
+++ app/services/authenticator.rb
@@ -5,7 +5,6 @@ class Authenticator
 
   def authenticate(raw_password)
     @user &&
-      !@user.suspended? &&
       @user.hashed_password &&
       BCrypt::Password.new(@user.hashed_password) == raw_password
   end
