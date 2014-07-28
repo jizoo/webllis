dbb5e8375dba4d586a86d3435137a48fe77cd12b
jizoo <jizoo2200@gmail.com>
2014-07-28 20:03:23 +0900

    Remove gravatar_for method

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -2,6 +2,7 @@
   User.create!(
     name: "example#{n+1}",
     email: "example#{n+1}@example.com",
-    password: 'password'
+    password: 'password',
+    icon_image: "https://secure.gravatar.com/avatar?s=50"
   )
 end
