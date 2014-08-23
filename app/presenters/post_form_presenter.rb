12bf81722d150b5d7109827dc4ea22f281b668cc
jizoo <jizoo2200@gmail.com>
2014-08-24 00:03:59 +0900

    Refactor Presenters

--- app/presenters/post_form_presenter.rb
+++ app/presenters/post_form_presenter.rb
@@ -2,13 +2,6 @@ class PostFormPresenter < FormPresenter
   delegate :image_tag, to: :view_context
   delegate :image, to: :object
 
-  def url_field_block(name, label_text, options = {})
-    markup(:div, class: 'form-group') do |m|
-      m << decorated_label(name, label_text, options.merge(class: 'control-label'))
-      m << url_field(name, options.merge(class: 'form-control'))
-    end
-  end
-
   def image_field_block(name, label_text)
     markup(:div, class: 'form-group') do |m|
       if object.image?
@@ -25,13 +18,6 @@ class PostFormPresenter < FormPresenter
           m << label("remove_#{name}", '削除する')
         end
       end
-    end
-  end
-
-  def tag_field_block(name, label_text)
-    markup(:div, class: 'form-group') do |m|
-      m << label(name, label_text, class: 'control-label')
-      m << text_field(name, class: 'form-control')
     end
   end
 end
