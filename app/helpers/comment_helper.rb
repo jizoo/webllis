4e622c0aeb6fe90c64f12c0cf90a926bb7a63b2e
jizoo <jizoo2200@gmail.com>
2014-08-23 16:27:44 +0900

    Refactor CommentsView

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -8,4 +8,23 @@ module CommentHelper
       comment.created_at.strftime('%Y/%m/%d %H:%M')
     end
   end
+
+  def comment_type(comment)
+    case comment.type
+    when 'sent'
+      'コメント'
+    when 'replied'
+      '返信'
+    else
+      raise
+    end
+  end
+
+  def truncated_content(comment)
+    truncate comment.content, length: 20
+  end
+
+  def link_to_commented_post(comment)
+    link_to(comment.post.title, comment.post, target: '_blank')
+  end
 end
