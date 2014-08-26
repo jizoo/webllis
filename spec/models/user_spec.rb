6535ea1249bc35ff89d7a499777f745e042f5cd3
jizoo <jizoo2200@gmail.com>
2014-08-26 21:37:41 +0900

    Clean up test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,111 +1,198 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe '#password' do
-    example '文字列を与えると、hashed_passwordは長さ60の文字列になる' do
-      user = User.new
-      user.password = 'webllis'
-      expect(user.hashed_password).to be_kind_of(String)
-
-      expect(user.hashed_password.size).to eq(60)
-    end
-
-    example 'nilを与えると、hashed_passwordはnilになる' do
-      user = User.new(hashed_password: 'x')
-      user.hashed_password = nil
-      expect(user.hashed_password).to be_nil
-    end
-  end
-
   describe '値の正規化' do
-    example 'email前後の空白を除去' do
-      user = create(:user, email: ' test@example.com ')
-      expect(user.email).to eq('test@example.com')
+    describe 'email' do
+      it '前後の半角スペースを除去' do
+        user = create(:user, email: ' test@example.com ')
+        expect(user.email).to eq('test@example.com')
+      end
+
+      it '前後の全角スペースを除去' do
+        user = create(:user, email: '　test@example.com　')
+        expect(user.email).to eq('test@example.com')
+      end
+
+      it '全角英数字記号を半角に変換' do
+        user = create(:user, email: 'ｔｅｓｔ＠ｅｘａｍｐｌｅ．ｃｏｍ')
+        expect(user.email).to eq('test@example.com')
+      end
+
+      it '大文字を小文字に変換しemail_for_indexに格納すること' do
+        user = create(:user, email: 'TEST＠EXAMPLE.COM')
+        expect(user.email_for_index).to eq('test@example.com')
+      end
     end
 
-    example 'emailに含まれる全角英数字記号を半角に変換' do
-      user = create(:user, email: 'ｔｅｓｔ＠ｅｘａｍｐｌｅ．ｃｏｍ')
-      expect(user.email).to eq('test@example.com')
-    end
+    describe 'name' do
+      it '前後の半角スペースを除去' do
+        user = create(:user, name: ' test ')
+        expect(user.name).to eq('test')
+      end
 
-    example 'email前後の全角スペースを除去' do
-      user = create(:user, email: "\u{3000}test@example.com\u{3000}")
-      expect(user.email).to eq('test@example.com')
+      it '前後の全角スペースを除去' do
+        user = create(:user, name: '　test　')
+        expect(user.name).to eq('test')
+      end
+
+      it '全角英数字記号を半角に変換' do
+        user = create(:user, name: 'ｔｅｓｔ')
+        expect(user.name).to eq('test')
+      end
     end
   end
 
   describe 'バリデーション' do
-    example '@を2個含むemailは無効' do
-      user = build(:user, email: 'test@@example.com')
-      expect(user).not_to be_valid
+    before { create(:user) }
+
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:name) }
+    it { should ensure_length_of(:name).is_within(3..50) }
+    it { should ensure_length_of(:password).is_within(6..20) }
+
+    describe 'email' do
+      it '「test@example.com」なら有効' do
+        user = build(:user, email: 'test@example.com')
+        expect(user).to be_valid
+      end
+
+      it 'ハイフン「-」を含むなら有効' do
+        user = build(:user, email: 'info-test@example.com')
+        expect(user).to be_valid
+      end
+
+      it 'プラス「＋」を含むなら有効' do
+        user = build(:user, email: 'info+test@example.com')
+        expect(user).to be_valid
+      end
+
+      it "クオート「'」を含むなら有効" do
+        user = build(:user, email: "infotest@example.com")
+        expect(user).to be_valid
+      end
+
+      it 'アット「@」を2個含むなら無効' do
+        user = build(:user, email: 'test@@example.com')
+        expect(user).to be_invalid
+      end
+
+      it '先頭が「mailto:」なら無効' do
+        user = build(:user, email: 'mailto:test@@example.com')
+        expect(user).to be_invalid
+      end
+
+      it '途中のスペースを含むなら無効' do
+        user = build(:user, email: 'te st@example.com')
+        expect(user).to be_invalid
+      end
     end
 
-    example '記号を含むnameは無効' do
-      user = build(:user, name: 'test-test')
-      expect(user).not_to be_valid
+    describe 'name' do
+      it '大文字を含むなら有効' do
+        user = build(:user, name: 'Test')
+        expect(user).to be_valid
+      end
+
+      it '数字を含むなら有効' do
+        user = build(:user, name: 'test1')
+        expect(user).to be_valid
+      end
+
+      it 'アンダースコア「_」を含むなら有効' do
+        user = build(:user, name: 'test_user')
+        expect(user).to be_valid
+      end
+
+      it 'ハイフン「-」を含むなら無効' do
+        user = build(:user, name: 'test-user')
+        expect(user).to be_invalid
+      end
+
+      it '記号を含むなら無効' do
+        user = build(:user, name: 'test☆')
+        expect(user).to be_invalid
+      end
+
+      it '日本語を含むなら無効' do
+        user = build(:user, name: 'テスト')
+        expect(user).to be_invalid
+      end
+
+      it '途中にスペースを含むなら無効' do
+        user = build(:user, name: 'te st')
+        expect(user).to be_invalid
+      end
     end
 
-    example '大文字を含むnameは有効' do
-      user = build(:user, name: 'Test')
-      expect(user).to be_valid
-    end
+    describe 'password' do
+      it 'password_confirmationと一致するなら有効' do
+        user = build(:user, password_confirmation: 'password')
+        expect(user).to be_valid
+      end
 
-    example 'アンダースコアを含むnameは有効' do
-      user = build(:user, name: 'test1')
-      expect(user).to be_valid
-    end
-
-    example 'アンダースコアを含むnameは有効' do
-      user = build(:user, name: 'test_test')
-      expect(user).to be_valid
-    end
-
-    example '他のユーザのメールアドレスと重複したemailは無効' do
-      user1 = create(:user)
-      user2 = build(:user, email: user1.email)
-      expect(user2).not_to be_valid
-    end
-
-    example 'password_confirmationと一致しないpasswordは無効' do
-      user = build(:user, password_confirmation: 'mismatch')
-      expect(user).not_to be_valid
+      it 'password_confirmationと一致しないなら無効' do
+        user = build(:user, password_confirmation: 'mismatch')
+        expect(user).to be_invalid
+      end
     end
   end
 
-  describe '#add_favorite!' do
-    let!(:user) { create(:user) }
-    let!(:post) { create(:post) }
+  describe '#password' do
+    context '文字列を与えた場合' do
+      it 'hashed_passwordは長さ60の文字列になること' do
+        user = build(:user, password: 'password')
+        expect(user.hashed_password).to be_kind_of(String)
+
+        expect(user.hashed_password.size).to eq(60)
+      end
+    end
+
+    context 'nilを与えた場合' do
+      it 'hashed_passwordはnilになること' do
+        user = build(:user, hashed_password: nil)
+        expect(user.hashed_password).to be_nil
+      end
+    end
+  end
+
+  describe '#add_favorite' do
+    let(:user) { create(:user) }
+    let(:post) { create(:post) }
 
     before do
       user.add_favorite!(post)
     end
 
-    example 'お気に入りに追加すると、userとpostの関係が作成される' do
+    it 'userとpostの関係が作成されること' do
       expect(user.favorite_posts).to be_include(post)
     end
 
-    example 'ユーザが削除されると、userとpostの関係は削除される' do
-      # TODO
-      # user.destroy
-      # expect(Favorite.where(user_id: user.id, post_id: post.id)).to be_empty
+    context 'ユーザが削除された場合' do
+      it 'userとpostの関係は削除されること' do
+        user.destroy
+        expect(Favorite.where(user: user, post: post)).to be_empty
+      end
     end
 
-    example '投稿が削除されると、userとpostの関係は削除される' do
-      post.destroy
-      expect(Favorite.where(user_id: user.id, post_id: post.id)).to be_empty
+    context '投稿が削除された場合' do
+      it 'userとpostの関係は削除されること' do
+        post.destroy
+        expect(Favorite.where(user: user, post: post)).to be_empty
+      end
     end
   end
 
-  describe '#remove_favorite!' do
+  describe '#remove_favorite' do
     let(:user) { create(:user) }
     let(:post) { create(:post) }
 
     before do
-      Favorite.create!(user_id: user.id, post_id: post.id)
+      Favorite.create!(user: user, post: post)
       user.remove_favorite!(post)
     end
 
-    example 'お気に入りを解除すると、userとpostの関係が解除される' do
+    it 'userとpostの関係が解除されること' do
       expect(user.favorite_posts).not_to be_include(post)
     end
   end
