641d1e8ff935601d96e0cb030f6194ef9d91438a
jizoo <jizoo2200@gmail.com>
2014-08-26 20:22:34 +0900

    Set up Database Cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'shoulda/matchers'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -20,7 +21,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
@@ -33,5 +34,18 @@ RSpec.configure do |config|
 
   config.after do
     Rails.application.config.webllis[:restrict_ip_addresses] = false
+  end
+  
+  config.before :each do
+    if Capybara.current_driver == :selenium
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
