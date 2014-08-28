190b88abb5343ec8d00e5db9eefbab8d8c4c2a7a
jizoo <jizoo2200@gmail.com>
2014-08-28 17:39:59 +0900

    Clean up test for Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,36 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Comment do
   describe '値の正規化' do
-    example 'content前後の空白を除去' do
-      comment = create(:comment, content: ' comment ')
-      expect(comment.content).to eq('comment')
-    end
+    describe 'content' do
+      it '前後の半角スペースを除去' do
+        comment = create(:comment, content: ' comment ')
+        expect(comment.content).to eq('comment')
+      end
 
-    example 'contentに含まれる全角英数字記号を半角に変換' do
-      comment = create(:comment, content: 'ｃｏｍｍｅｎｔ')
-      expect(comment.content).to eq('comment')
-    end
+      it '前後の全角スペースを除去' do
+        comment = create(:comment, content: '　comment　')
+        expect(comment.content).to eq('comment')
+      end
 
-    example 'content途中の全角スペースを半角に変換' do
-      comment = create(:comment, content: "reply\u{3000}comment")
-      expect(comment.content).to eq('reply comment')
-    end
+      it '途中の全角スペースを半角に変換' do
+        comment = create(:comment, content: 'reply　comment')
+        expect(comment.content).to eq('reply comment')
+      end
 
-    example 'content前後の全角スペースを除去' do
-      comment = create(:comment, content: "\u{3000}comment\u{3000}")
-      expect(comment.content).to eq('comment')
+      it '含まれる全角英数字記号を半角に変換' do
+        comment = create(:comment, content: 'ｃｏｍｍｅｎｔ')
+        expect(comment.content).to eq('comment')
+      end
     end
   end
 
   describe 'バリデーション' do
-    example '空白のcontentは無効' do
-      comment = build(:comment, content: '')
-      expect(comment).not_to be_valid
-    end
+    before { create(:comment) }
 
-    example '401文字以上のcontentは無効' do
-      comment = build(:comment, content: 'a'  * 401)
-      expect(comment).not_to be_valid
-    end
+    it { should validate_presence_of(:content) }
+    it { should ensure_length_of(:content).is_within(0..400) }
   end
 end
